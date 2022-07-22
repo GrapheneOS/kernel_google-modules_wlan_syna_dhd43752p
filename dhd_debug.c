52ea3111864a876db91ff606e50bba110c314d0e
Terry Chen <terry.chen@synaptics.corp-partner.google.com>
2022-07-22 13:49:02 +0800

    DHD: void calling copy_to_user() under atomic context
    The root cause is that 'copy_to_user()' was got called under atomic context. 'copy_to_user()' function may sleep. So, it should not be called under spinlock is held. To handle this, 'spin_lock_unlock()' moved into location before 'copy_to_user()' calling.
    
    Bug: 238901573
    Test: Build Pass, basic function works fine.
    
    Signed-off-by: Terry Chen <terry.chen@synaptics.corp-partner.google.com>
    Change-Id: I02d474894a23822f013f0346835c7887135888b3

--- dhd_debug.c
+++ dhd_debug.c
@@ -2306,6 +2306,7 @@ dhd_dbg_monitor_get_tx_pkts(dhd_pub_t *dhdp, void __user *user_buf,
 	tx_report = dhdp->dbg->pkt_mon.tx_report;
 	tx_pkt = tx_report->tx_pkts;
 	pkt_count = MIN(req_count, tx_report->status_pos);
+	DHD_PKT_MON_UNLOCK(dhdp->dbg->pkt_mon_lock, flags);
 
 #ifdef CONFIG_COMPAT
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0))
@@ -2356,7 +2357,6 @@ dhd_dbg_monitor_get_tx_pkts(dhd_pub_t *dhdp, void __user *user_buf,
 	}
 	*resp_count = pkt_count;
 
-	DHD_PKT_MON_UNLOCK(dhdp->dbg->pkt_mon_lock, flags);
 	if (!pkt_count) {
 		DHD_ERROR(("%s(): no tx_status in tx completion messages, "
 			"make sure that 'd11status' is enabled in firmware, "
@@ -2400,6 +2400,7 @@ dhd_dbg_monitor_get_rx_pkts(dhd_pub_t *dhdp, void __user *user_buf,
 	rx_report = dhdp->dbg->pkt_mon.rx_report;
 	rx_pkt = rx_report->rx_pkts;
 	pkt_count = MIN(req_count, rx_report->pkt_pos);
+	DHD_PKT_MON_UNLOCK(dhdp->dbg->pkt_mon_lock, flags);
 
 #ifdef CONFIG_COMPAT
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0))
@@ -2451,7 +2452,6 @@ dhd_dbg_monitor_get_rx_pkts(dhd_pub_t *dhdp, void __user *user_buf,
 	}
 
 	*resp_count = pkt_count;
-	DHD_PKT_MON_UNLOCK(dhdp->dbg->pkt_mon_lock, flags);
 
 	return BCME_OK;
 }
